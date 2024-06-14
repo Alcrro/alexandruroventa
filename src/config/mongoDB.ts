596439cdfd8ecf4eb6bde01dd0a1648c1f33cb79
AlexandruRoventa <alex.roventa94@gmail.com>
2024-06-14 21:43:29 +0300

    newV3

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -2,8 +2,13 @@ import mongoose, { connection } from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log(`${process.env.MONGO_URI}`);
+
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}`!);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}`!, {
+      useNewUrlParser: "true",
+      useUnifiedTopology: "true",
+    });
     connection.on("Connected", () => {
       console.log(`MongoDB Connected: ${conn.connection.host}`);
     });
