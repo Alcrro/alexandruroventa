d553a7c4e86bf781605f312d642dced8b56510c4
AlexandruRoventa <alex.roventa94@gmail.com>
2024-06-14 21:46:33 +0300

    newV4

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -5,7 +5,7 @@ export const connectDB = async () => {
     console.log(`${process.env.MONGO_URI}`);
 
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}`!, {
+    const conn = await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: "true",
       useUnifiedTopology: "true",
     });
