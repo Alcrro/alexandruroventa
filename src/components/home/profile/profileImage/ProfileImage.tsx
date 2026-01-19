f793becabd3fd2897393741b56cf30de712796be
Alexandru Roventa <96865252+Alcrro@users.noreply.github.com>
2026-01-19 20:20:04 +0200

    Change profile image to a new URL
    Updated profile image source URL.

--- src/components/home/profile/profileImage/ProfileImage.tsx
+++ src/components/home/profile/profileImage/ProfileImage.tsx
@@ -8,7 +8,7 @@ export default function ProfileImage() {
     <div className="profile-container">
       <div className="profile-image-container">
         <Image
-          src={"https://alexandru-roventa.s3.eu-central-1.amazonaws.com/alexandru-roventa-image.png"}
+          src={"https://alexandru-roventa.s3.eu-central-1.amazonaws.com/IMG_20260116_115741_286.webp"}
           alt="my-image"
           width={500}
           height={500}
@@ -20,3 +20,4 @@ export default function ProfileImage() {
     </div>
   );
 }
+
